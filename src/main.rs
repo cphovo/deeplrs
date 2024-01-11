414f7c01dd528a28010738698d3d874342bcedb8
cphovo <cphovo@qq.com>
2024-01-11 17:00:13 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,17 @@ async fn main() {
     let mut target = "en";
     if args.len() == 2 {
         let lang_info = detect(text).unwrap();
-        // 如果检测语言不是普通话，且为指定语言
+        // 如果检测语言不是普通话，且未指定语言
         // 一律视为英文翻译为中文
-        if lang_info.lang() != Lang::Cmn {
+        // 包含英文的普通话会被检测为 Tgl，检测的可信度小于 0.2 认为不可信
+        if lang_info.lang() != Lang::Cmn
+            && lang_info.lang() != Lang::Tgl
+            && lang_info.confidence() > 0.2
+        {
             source = "en";
             target = "zh";
         }
+        println!("{:?}", lang_info);
     }
 
     if args.len() == 3 {
